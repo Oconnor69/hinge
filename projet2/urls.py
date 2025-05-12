afa3d9c3958cc2894f874b30734836fdb1993492
unknown <walid2@example.com>
2025-05-12 12:38:18 +0100

    Premier envoi du projet complet

--- projet2/urls.py
+++ projet2/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rec.urls')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
